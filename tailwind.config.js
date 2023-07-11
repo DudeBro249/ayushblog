2038af980fe8803033dd35bc4b1e724ba4508438
DudeBro249 <appdevdeploy@gmail.com>
2023-07-10 17:40:12 -0700

    :sparkles: feat: add landing page

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    content: ["./layouts/**/*.{html,js}"],
+    theme: {
+      extend: {
+        colors: {
+          'main_black': '#212121',
+          'darker': '#121212'
+        },
+        fontFamily: {
+          'inter': ['Inter', 'sans-serif']
+        }
+      }
+    },
+    plugins: [],
+}
