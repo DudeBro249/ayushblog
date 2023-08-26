19207beea05b61c758d29749b205a8a5297359b3
DudeBro249 <appdevdeploy@gmail.com>
2023-08-26 12:38:27 -0400

    :lipstick: style: rename colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,10 @@ module.exports = {
       extend: {
         colors: {
           'main_black': '#212121',
-          'darker': '#121212',
+          'darker_black': '#121212',
           'admonition_info': '#54C7EC',
           'admonition_caution': '#F6BB41',
+          'developer_night': '#282A36'
         },
         fontFamily: {
           'inter': ['Inter', 'sans-serif']
